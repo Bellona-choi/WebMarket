c5c5afea594063943d7c3381c740e3adcfe97520
최재원 <jaewon3588@naver.com>
2022-06-13 15:37:12 +0900

    5장 상품 정보 화면

--- src/main/java/com/survivalcoding/data/ProductRepository.java
+++ src/main/java/com/survivalcoding/data/ProductRepository.java
@@ -1,18 +1,15 @@
 package com.survivalcoding.data;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-
 import com.survivalcoding.domain.model.Product;
 
+// 다형성
+public class ProductRepository {
+    private List<Product> products = new ArrayList<>();
 
-
-public class ProductRepository{
-	private List<Product> products = new ArrayList<>();
-	
-	public ProductRepository() {
-		Product phone = new Product("P1234", "iPhone 6s", 800000);
+    public ProductRepository() {
+        Product phone = new Product("P1234", "iPhone 6s", 800000);
         phone.setDescription("4.7-inch, 1334x750 Retina HD display");
         phone.setCategory("Smart Phone");
         phone.setManufacturer("Apple");
@@ -36,22 +33,19 @@ public class ProductRepository{
         products.add(phone);
         products.add(notebook);
         products.add(tablet);
-	}
-	//다형성
-	//public static void main(String[] args) {
-		// 삽입, 삭제가 빈번할 때 항상 동일 성능, 하지만 ArrayList보다 검색이 느리다.
-		// 메모리 좀더 많이 차지한다.
-		//List<Product> products = new LinkedList<>();
-		
-		// 크기가 커질수록 십입, 삭제가 느려짐, 내부적으로 배열이라 성능이 빠르다.
-		//List<Product> products2 = new ArrayList<>();
-		
-		
-	//}
-	//public static ArrayList<Product> sort(List<Product> products){
-		//return new ArrayList<>();
-	//}
-	public List<Product> getAllProducts() {
+    }
+
+    public List<Product> getAllProducts() {
         return products;
     }
+
+    public Product getProductById(String id) {
+        // p.173 참고
+        // List -> Stream (데이터의 흐름)
+        return products.stream()
+                .filter((product) -> product.getId().equals(id)) // 조건
+                .findFirst() // 첫번째
+                .get(); // 얻어
+    }
+
 }
