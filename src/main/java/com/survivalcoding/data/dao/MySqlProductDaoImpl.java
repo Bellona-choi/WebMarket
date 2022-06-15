b09dbc8dabbc88368ee1bf0503cb7821e8a40779
최재원 <jaewon3588@naver.com>
2022-06-15 15:05:41 +0900

    update, delete DB 기능 작성

--- src/main/java/com/survivalcoding/data/dao/MySqlProductDaoImpl.java
+++ src/main/java/com/survivalcoding/data/dao/MySqlProductDaoImpl.java
@@ -1,8 +1,8 @@
 package com.survivalcoding.data.dao;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -11,67 +11,109 @@ import java.util.List;
 
 import com.survivalcoding.domain.model.Product;
 
-public class MySqlProductDaoImpl implements ProductDao{
+public class MySqlProductDaoImpl implements ProductDao {
+//	private final static String TABLE_NAME = "product";
+//	private final static String COLUMN_ID = "p_id";
+	
+	public MySqlProductDaoImpl() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("jdbc 드라이버 로드 실패");
+		}
+	}
 
-   public MySqlProductDaoImpl() {
-      try {
-         Class.forName("com.mysql.cj.jdbc.Driver");
-      } catch (ClassNotFoundException e) {
-         throw new IllegalStateException("jdbc 드라이버 로드 실패");
-      }
-   }
-   
-   @Override
-   public List<Product> getAll() {
-      // TODO Auto-generated method stub
-       List<Product> results = new ArrayList<>();
-      
-      String sql = "SELECT * FROM product";
-      try (Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.23.103:33060/kopoctc", "kopo43", "1234");
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);){
-         while(rs.next()) {
-            String id = rs.getString("p_id");
-            String name = rs.getString("p_name");
-            int unitPrice = rs.getInt("p_unitPrice");
-            String descripstion = rs.getString("p_description");
-            String category = rs.getString("p_category");
-            String manufacturer = rs.getString("p_menufacturer");
-            long unitsInStock = rs.getLong("p_unitsInStock");
-            String condition = rs.getString("p_condition");
-            
-            Product product = new Product (id, name, unitPrice);
-            product.setDescription(descripstion);
-            product.setCategory(category);
-            product.setManufacturer(manufacturer);
-            product.setUnitsInStock(unitsInStock);
-            product.setCondition(condition);
-            
-            results.add(product);
-         }
-         
-      }catch(SQLException e){
-         throw new IllegalStateException("db 연결 실패" + e.getMessage());
-      }
-      return results;
-   }
+	@Override
+	public List<Product> getAll() {
+		// TODO Auto-generated method stub
+		List<Product> results = new ArrayList<>();
 
-   @Override
-   public void insert(Product product) {
-      // TODO Auto-generated method stub
-      
-   }
+		String sql = "SELECT * FROM product";
+		try (Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.23.103:33060/kopoctc", "kopo43",
+				"1234"); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql);) {
+			while (rs.next()) {
+				String id = rs.getString("p_id");
+				String name = rs.getString("p_name");
+				int unitPrice = rs.getInt("p_unitPrice");
+				String descripstion = rs.getString("p_description");
+				String category = rs.getString("p_category");
+				String manufacturer = rs.getString("p_menufacturer");
+				long unitsInStock = rs.getLong("p_unitsInStock");
+				String condition = rs.getString("p_condition");
 
-   @Override
-   public void update(Product product) {
-      // TODO Auto-generated method stub
-      
-   }
+				Product product = new Product(id, name, unitPrice);
+				product.setDescription(descripstion);
+				product.setCategory(category);
+				product.setManufacturer(manufacturer);
+				product.setUnitsInStock(unitsInStock);
+				product.setCondition(condition);
 
-   @Override
-   public void delete(Product product) {
-      // TODO Auto-generated method stub
-      
-   }
+				results.add(product);
+			}
+
+		} catch (SQLException e) {
+			throw new IllegalStateException("db 연결 실패" + e.getMessage());
+		}
+		return results;
+	}
+
+	@Override
+	public void insert(Product product) {
+		// PreparedStatement 동적 쿼리 생성 : Insert, update delete 여러번 할 때 고속
+		String sql = "INSERT INTO product VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn =
+        		DriverManager.getConnection("jdbc:mysql://192.168.23.103:33060/kopoctc", "kopo43", "1234");
+        		PreparedStatement stmt = conn.prepareStatement(sql);) {
+            stmt.setString(1, product.getId());
+            stmt.setString(2, product.getName());
+            stmt.setInt(3, product.getUnitPrice());
+            stmt.setString(4, product.getDescription());
+            stmt.setString(5, product.getCategory());
+            stmt.setString(6, product.getManufacturer());
+            stmt.setLong(7, product.getUnitsInStock());
+            stmt.setString(8, product.getCondition());
+            stmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new IllegalStateException("insert 실패" + e.getMessage());
+		}
+
+	}
+
+	@Override
+	public void update(Product product) {
+		String sql = "UPDATE ? SET p_name=?, p_unitPrice=?, p_description=?, p_category=?, p_menufacturer=?, p_unitsInStock=?, p_condition=?";
+        try (Connection conn =
+        		DriverManager.getConnection("jdbc:mysql://192.168.23.103:33060/kopoctc", "kopo43", "1234");
+        		PreparedStatement stmt = conn.prepareStatement(sql);) {
+            stmt.setString(1, TABLE_NAME);
+            stmt.setString(2, product.getName());
+            stmt.setInt(3, product.getUnitPrice());
+            stmt.setString(4, product.getDescription());
+            stmt.setString(5, product.getCategory());
+            stmt.setString(6, product.getManufacturer());
+            stmt.setLong(7, product.getUnitsInStock());
+            stmt.setString(8, product.getCondition());
+            stmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new IllegalStateException("update 실패" + e.getMessage());
+		}
+
+	}
+
+	@Override
+	public void delete(Product product) {
+		String sql = "DELETE FROM ? WHERE p_id=?";
+        try (Connection conn =
+        		DriverManager.getConnection("jdbc:mysql://192.168.23.103:33060/kopoctc", "kopo43", "1234");
+                PreparedStatement stmt = conn.prepareStatement(sql);) {
+            stmt.setString(1, TABLE_NAME);
+            stmt.setString(2, product.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+           throw new IllegalStateException("delete 실패 " + e.getMessage());
+        }
+    }
 
 }
--- src/main/java/com/survivalcoding/data/dao/ProductDao.java
+++ src/main/java/com/survivalcoding/data/dao/ProductDao.java
@@ -7,7 +7,10 @@ import com.survivalcoding.domain.model.Product;
 // DB랑 연결할 놈
 public interface ProductDao {
     // interface 안에서는 public 생략되어 있고 public 만 됨
-    List<Product> getAll();
+	String TABLE_NAME = "product";
+	String COLUMN_ID = "p_id";
+	
+	List<Product> getAll();
 
     void insert(Product product);
 
